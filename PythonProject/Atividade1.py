ac17ac1729e69ff148417676469aed836d798b01
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2026-03-18 15:29:28 -0300

    aula1

---
+++ PythonProject/Atividade1.py
@@ -0,0 +1,27 @@
+#1 ler calcular e escrever a media
+n1 = int(input('Digite o primero número: '))
+n2 = int(input('Digite o segundo número: '))
+med = ((n1 + n2)/2)
+print ('A média destes números é : ', med)
+
+
+#2 ler dois valores e multiplicar
+x = int(input('Digite o multiplicador :'))
+y = int(input('Digite o multiplicado :'))
+mult = (x*y)
+print('O resultado da mutiplicação é :', mult)
+
+#3 Ler e calcular soma, produto, divisao e multiplicação
+num1 = int(input('Digite o primeiro número : '))
+num2 = int(input('Digite o primeiro número : '))
+print ('A soma destes números é : ',num1+num2 )
+print ('O produto destes números é : ',num1-num2 )
+print ('A multiplicação destes números é : ',num1*num2 )
+print ('A divisor destes números é : ',num1/num2 )
+
+#calcular volume
+comp = int(input('Digite o comprimento da caixa :'))
+larg = int(input('Digite a largura da caixa :'))
+altura = int(input('Digite a altura da caixa :'))
+vol = comp*larg*altura
+print('o volume da caixa é de: ',vol)
---
+++ PythonProject/aula1.py
@@ -0,0 +1,16 @@
+print('a soma de {0} + {1} é {2}'.format(1,2, 1+2))
+#soma
+numero1 = 10
+numero2 = 20
+soma = numero1 + numero2
+print(soma)
+
+idade = input('Digite sua idade: ')
+print('a idade informada é:', idade)
+
+n1 = input('digite o primero numero: ')
+n2 = input('digite o segundo numero: ')
+num1 = int(n1)
+num2 = int(n2)
+soma2 = num1 + num2
+print(soma2)
